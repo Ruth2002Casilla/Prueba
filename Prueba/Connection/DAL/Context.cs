b72d2249cf90f96245091b48fc145d59013e1807
Ruth Betsaida Casilla Garcia <ruth_casilla@ucne.edu.do>
2024-03-27 19:52:28 -0400

    Agregando Dependencias y La conexion a la Base de Datos

---
+++ Prueba/Connection/DAL/Context.cs
@@ -0,0 +1,20 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Connection.Dal
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>options) : base(options){ }
+
+        public DbSet<Asociacion> Asociacion { get; set; }
+        public DbSet<Bloques> Bloque { get; set;}
+        public DbSet<Caja> Caja { get; set; }
+        public DbSet<DetalleCaja> DetalleCaja { get; set; }
+        public DbSet<DetalleRegante> DetalleRegante { get; set; }
+        public DbSet<ParametroOperacionales> ParametroOperacionales { get; set; }
+        public DbSet<Parcela> Parcela { get; set; }
+        public DbSet<Regantes> Regantes { get; set; }
+        public DbSet<Usuarios> Usuario { get; set; }
+    }
+}
